d942c5250d5725979de3dee49caf35db837c5b3c
Abdulmajed Dakkak <adakkak@eng.utoledo.com>
2008-11-03 12:11:42 -0500

    wrote abstract

---
+++ examples/run.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+ruby 100cubes.rb > 100cubes.x3d
+ruby box.rb > box.x3d
+ruby collision.rb > collision.x3d
+ruby directional_light.rb > direction_light.x3d
+ruby point_light.rb > point_light.x3d
+ruby radiohead.rb > radiohead.x3d
+ruby shapes.rb > shapes.x3d
+ruby procedural_textures.rb > procedural_textures.x3d
